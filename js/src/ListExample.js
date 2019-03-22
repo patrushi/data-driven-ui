6f461593e6c6021dabf9c41c27ec488b21f04581
Павел Трушин <ptrushin@custis.ru>
2019-03-22 12:11:15 +0300

    Initital

--- js/src/ListExample.js
+++ js/src/ListExample.js
@@ -13,35 +13,25 @@ export default class ListExample extends PureComponent {
             ],
             key: 'FirstName',
             filters: [
-               {name: 'Gender', type: 'shortselect', options: [{key: 'Male'}, {key: 'Female'}], dataSourse: {refresh: 'debounce', type: 'Microsoft.OData.Service.Sample.TrippinInMemory.Models.PersonGender', func: (name, value) => {return `Gender eq Microsoft.OData.Service.Sample.TrippinInMemory.Models.PersonGender'${value}'`}}},
-               {name: 'FirstName', type: 'text', dataSourse: {refresh: 'debounce'}}
+               {name: 'Gender', type: 'shortselect', options: [{key: 'Male'}, {key: 'Female'}], dataSourse: {type: 'Microsoft.OData.Service.Sample.TrippinInMemory.Models.PersonGender', func: (name, value) => {return `Gender eq Microsoft.OData.Service.Sample.TrippinInMemory.Models.PersonGender'${value}'`}}},
+               {name: 'FirstName', type: 'text', dataSourse: {refresh: 'debounce'}},
+               {name: 'LastName', type: 'text', dataSourse: {refresh: 'debounce'}}
             ],
+            //filtersLayout: {type: 'default', perLine: 2},
             paging: {},
             selectable: {type: 'row&checkbox', isMulti: false},
             dataSourse: {type: 'odata', shortPath: `People`, selectAll: true}
         };
 
-        /* this.metadata = {
-            columns: [
-                {name: 'FirstName', isOrderable: true},
-                {name: 'LastName', title: 'Last Name'},
-            ],
-            filters: {
-               Gender: {type: 'shortselect', options: [{key: 'Male'}, {key: 'Female'}], dataSourse: {debounceInterval: 200, func: (name, value) => {return `Gender eq Microsoft.OData.Service.Sample.TrippinInMemory.Models.PersonGender'${value}'`}}}
-            },
-            paging: {},
-            dataSourse: {type: 'local', shortPath: `People`}
-        }; */
-
         this.state = {};
     }
 
     onSelect = (isSelect, lastSelect, allSelect) => {
-        console.log(isSelect, lastSelect, allSelect);
+        //console.log(isSelect, lastSelect, allSelect);
     }
 
     onSingleSelect = (select) => {
-        console.log(select);
+        //console.log(select);
     }
 
     render() {
--- js/src/default-ui/FilterPanel.js
+++ js/src/default-ui/FilterPanel.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import Grid from '@material-ui/core/Grid';
 
 export default class FilterPanel extends PureComponent {
 
@@ -15,10 +16,35 @@ export default class FilterPanel extends PureComponent {
     }
 
     render() {
-        return <React.Fragment>
-            {this.props.meta.filters.map((item, idx) => (
-                <div key={idx}>{this.getField(item)}</div>
-            ))}
-        </React.Fragment>;
+        var {filtersLayout} = this.props.meta;
+
+        if (!filtersLayout || !filtersLayout.type || filtersLayout.type === 'default')
+        {
+            var perLine = (filtersLayout && filtersLayout.perLine) || 3;
+            var linesCnt = Math.floor((this.props.meta.filters.length - 1)/perLine) + 1;
+            var filterLines = [];
+            for (let i = 0; i < linesCnt; i++)
+            {
+                filterLines.push(this.props.meta.filters.slice(i * perLine, (i + 1) * perLine));
+            }
+            var empty = [];
+            if (linesCnt > 1) for (let i = 0; i < perLine * linesCnt - this.props.meta.filters.length; i++)
+            {
+                empty.push(<Grid item xs />);
+            }
+            return <React.Fragment>
+                {
+                    filterLines.map((filters, rowIdx) => (
+                        <Grid container key={rowIdx} spacing={8}>
+                            {
+                                filters.map((filter, idx) => (
+                                    <Grid item xs key={idx}>{this.getField(filter)}</Grid>
+                                )).concat(rowIdx+1 === linesCnt ? empty : [])
+                            }
+                        </Grid>
+                    )
+                )}
+            </React.Fragment>;
+        }
     }
 }
