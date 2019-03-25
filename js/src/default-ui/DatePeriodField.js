7ed764217811de4063542e5e92c27a19498b8d5a
Павел Трушин <ptrushin@custis.ru>
2019-03-25 12:36:43 +0300

    Initital

--- js/src/default-ui/DatePeriodField.js
+++ js/src/default-ui/DatePeriodField.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import DateField from './DateField';
+import { IconButton } from "@material-ui/core";
+import { Remove as RemoveIcon } from "@material-ui/icons";
 
 export default class DatePeriodField extends PureComponent {
     render() {
@@ -12,7 +14,8 @@ export default class DatePeriodField extends PureComponent {
                     label={label || ' '}
                     value={value.from}
                     placeholder={placeholder}
-                    onChange={v => onChange({from: v, till: value.till})} /> - 
+                    onChange={v => onChange({from: v, till: value.till})} />
+                <RemoveIcon />
                 <DateField
                     {...rest}
                     value={value.till}
