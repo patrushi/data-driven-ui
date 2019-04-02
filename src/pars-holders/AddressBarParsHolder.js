0589cc2a7efea76f792ebf81aad0d9d283dca798
Павел Трушин <ptrushin@custis.ru>
2019-04-02 14:49:59 +0300

    Initial

--- src/pars-holders/AddressBarParsHolder.js
+++ src/pars-holders/AddressBarParsHolder.js
@@ -9,7 +9,7 @@ export default class AddressBarParsHolder {
         var pars = {};
         if (data.paging) {
             if (data.paging.page) pars.page = data.paging.page;
-            if (data.paging.perPage) pars.perPage = data.paging.perPage;
+            if (data.paging.perPage !== 10) pars.perPage = data.paging.perPage;
         }
         return pars;
     }
@@ -17,24 +17,22 @@ export default class AddressBarParsHolder {
     deserializePars = (meta, pars) => {
         if (!pars) return null;
         var data = {};
-        if (pars.page !== undefined || pars.perPage !== undefined) {
-            data.paging = {page: isNaN(pars.page) ? 0 : Number(pars.page), perPage: isNaN(pars.perPage) ? 10 : Number(pars.perPage)}
-        }
+        data.paging = {page: isNaN(pars.page) ? 0 : Number(pars.page), perPage: isNaN(pars.perPage) ? 10 : Number(pars.perPage)};
         return data;
     }
 
     savePars = (pars) => {
         if (this.props.meta.history == null) return;
-        let locationSearch = '?' + qs.stringify(pars);
+        let locationSearch = qs.stringify(pars);
+        locationSearch = locationSearch ? '?' + locationSearch : locationSearch;
         if (this.locationSearch === locationSearch) return;
         this.locationSearch = locationSearch;
-        let path = this.props.meta.history.location.pathname + locationSearch;
-        this.props.meta.history.push(path);
+        this.props.meta.history.push(this.props.meta.history.location.pathname + locationSearch);
     }
 
     loadPars = () => {
         if (this.props.meta.history == null) return;
-        if (this.props.meta.history.location.search === this.locationSearch) return null;
+        if (this.props.meta.history.location.search === this.locationSearch) return;
         this.locationSearch = this.props.meta.history.location.search;
         return qs.parse(this.locationSearch);
     }
