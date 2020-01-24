ed32a4a0f0f6782763047116c67768cbcb85cda9
Павел Трушин <ptrushin@custis.ru>
2020-01-24 16:01:58 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Demo from './examples/List/Demo.js';
+import SimpleList from './examples/SimpleList.js';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Route component={Demo} />
+                <Route component={SimpleList} />
             </BrowserRouter>
         );
     }
---
+++ src/examples/SimpleList.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from "react";
+import { List } from "../lib";
+import { DefaultUiMeta as GlobalMeta } from "../lib";
+import Paper from "@material-ui/core/Paper";
+import { withRouter } from "react-router-dom";
+
+export class ListExample extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.meta = {
+      columns: [
+        { name: "OrderID", orderable: true },
+        { name: "CustomerID" },
+        { name: "OrderDate", type: "date" },
+        { name: "RequiredDate", type: "date" },
+        { name: "ShippedDate", type: "date" },
+        { name: "ShipCountry" }
+      ],
+      key: "OrderID",
+      orderable: true,
+      filters: [
+        { name: "ShipCountry", type: "text" },
+        { name: "OrderDate", type: "dateperiod" }
+      ],
+      paging: {},
+      selectable: { type: "row&checkbox", isMulti: true },
+      parsHolder: {
+        type: "addressBar",
+        prefix: "",
+        history: this.props.history
+      },
+      dataSource: { type: "odata", shortPath: `Orders`, selectAll: true }
+    };
+  }
+
+  render() {
+    return (
+      <Paper style={{ margin: 15, padding: 15 }}>
+        <List
+          meta={this.meta}
+          title="Orders"
+          globalMeta={GlobalMeta}
+          onSelect={this.onSelect}
+          onSingleSelect={this.onSingleSelect}
+          parentProps={this.props}
+        />
+      </Paper>
+    );
+  }
+}
+
+export default withRouter(ListExample);
